68a7edacdeb27bbe77e0299406e817dbe7be1b84
arcadioreyes <109815910+arcadioreyes@users.noreply.github.com>
2023-02-09 22:02:47 +0100

    form added to SignUpPage

--- src/components/SignUpPage/SignUpPage.jsx
+++ src/components/SignUpPage/SignUpPage.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const SignUpPage = () => {
-  return <div>Welcome to the SignUpPage</div>;
+  return (
+    <div>
+      <h1>Welcome to the Sign Up Page Stroller!</h1>
+      <br />
+
+      <form>
+        <input type="text" name="email" />
+        <input type="text" name="username" />
+        <input type="password" name="password" />
+        <input type="text" name="country" />
+        <input type="text" name="city" />
+        <button type="submit">Create my account</button>
+      </form>
+    </div>
+  );
 };
 
 export default SignUpPage;
