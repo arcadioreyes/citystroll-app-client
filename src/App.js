c8e89f652278bbfe901533106233ff3d219bcf25
arcadioreyes <109815910+arcadioreyes@users.noreply.github.com>
2023-02-09 21:05:53 +0100

    HomePage, SignUpPage and LoginPage imported to app.js

--- src/App.js
+++ src/App.js
@@ -2,18 +2,23 @@ import "./App.scss";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Header from "./components/Header/Header";
-import css from './styles/app.module.scss'
+import css from "./styles/app.module.scss";
+import HomePage from "./components/HomePage/HomePage";
+import SignUpPage from "./components/SignUpPage/SignUpPage";
+import LoginPage from "./components/LoginPage/LoginPage";
 
- 
 function App() {
   return (
     <div className={`bg-primary ${css.container}`}>
-      <Header/>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/homepage" element={<HomePage />} />
+        <Route path="/signuppage" element={<SignUpPage />} />
+        <Route path="/loginpage" element={<LoginPage />} />
       </Routes>
     </div>
   );
 }
- 
+
 export default App;
