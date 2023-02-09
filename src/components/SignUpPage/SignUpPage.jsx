4f6922f44669719bbc7cdec2936cafaf8c7a6413
arcadioreyes <109815910+arcadioreyes@users.noreply.github.com>
2023-02-09 22:18:15 +0100

    added a useState and a handleChange to the form

--- src/components/SignUpPage/SignUpPage.jsx
+++ src/components/SignUpPage/SignUpPage.jsx
@@ -1,17 +1,52 @@
 import React from "react";
+import { useState } from "react";
 
 const SignUpPage = () => {
+  const [user, setUser] = useState({
+    email: "",
+    username: "",
+    password: "",
+    country: "",
+    city: "",
+  });
+
+  const handleChange = (e) => {};
   return (
     <div>
-      <h1>Welcome to the Sign Up Page Stroller!</h1>
+      <h1>Welcome Stroller to the Sign Up Page!</h1>
       <br />
 
       <form>
-        <input type="text" name="email" />
-        <input type="text" name="username" />
-        <input type="password" name="password" />
-        <input type="text" name="country" />
-        <input type="text" name="city" />
+        <input
+          type="text"
+          name="email"
+          value={user.email}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="username"
+          value={user.username}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="username"
+          value={user.username}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="country"
+          value={user.country}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="city"
+          value={user.city}
+          onChange={handleChange}
+        />
         <button type="submit">Create my account</button>
       </form>
     </div>
