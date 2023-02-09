12f249f4e873345d985d134229ce6a2e49f08f7f
arcadioreyes <109815910+arcadioreyes@users.noreply.github.com>
2023-02-09 23:46:53 +0100

    form at LoginPage now is working. After logged in user is directed to Home.

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,7 +1,77 @@
-import React from "react";
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
-  return <div> Welcome to the LoginPage</div>;
+  const [user, setUser] = useState({
+    email: "",
+    username: "",
+    password: "",
+  });
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    setUser({ ...user, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(user);
+    axios
+      .post("http://localhost:5005/auth/login", user)
+      .then((response) => {
+        console.log("token", response.data.authToken);
+        navigate("/");
+      })
+      .catch((err) => console.error(err));
+  };
+
+  return (
+    <>
+      <div>
+        <h1>Welcome to the LoginPage</h1>
+
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email">Email:</label>
+          <br />
+          <input
+            type="text"
+            name="email"
+            value={user.email}
+            onChange={handleChange}
+          />
+          <br />
+
+          <label htmlFor="username">Username:</label>
+          <br />
+          <input
+            type="text"
+            name="username"
+            value={user.username}
+            onChange={handleChange}
+          />
+          <br />
+
+          <label htmlFor="password">Password:</label>
+          <br />
+          <input
+            type="password"
+            name="password"
+            value={user.password}
+            onChange={handleChange}
+          />
+          <br />
+          <br />
+
+          <button type="submit">Login!</button>
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default LoginPage;
