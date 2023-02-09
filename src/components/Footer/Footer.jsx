2771e6f6e1747c994ae8cefa464ac7ffc53487c9
Axel <axel_clemente@yahoo.com>
2023-02-09 12:42:00 +0100

    Errors in files SCSS solved

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
-import css from './Footer.module.scss'
-import { motion } from 'framer-motion'
-import { staggerChildren } from '../../utils/motion'
-import { footerVariants } from '../../utils/motion'
+import css from './Footer.module.scss';
+import { motion } from 'framer-motion';
+import { staggerChildren } from '../../utils/motion';
+import { footerVariants } from '../../utils/motion';
 import { BsGithub } from "react-icons/bs";
+import { useState, useEffect }  from "react";
+import axios from 'axios';
 
 const Footer = () => {
+    const [strolls, setStrolls] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:5005/strolls')
+            .then((response) => {
+                console.log('response.data', response.data)
+                setStrolls(response.data)
+        })
+    },[]);
+
   return (
 
     <motion.section
@@ -15,7 +27,7 @@ const Footer = () => {
         viewport={{once: false, amount: 0.25}}
         className={`paddings ${css.wrapper}`}>
 
-        <a id="developers"></a>
+        <button id="developers"></button>
         <motion.div
             variants={footerVariants} 
             className={`innerWidth yPaddings flexCenter ${css.container}`}>
@@ -25,8 +37,8 @@ const Footer = () => {
                     Full Stack Developers: <br />
                 </span>
                 <span className="primaryTextDevelop">
-                    <a href="https://github.com/axelclemente?tab=repositories" target="_blank">Axel Clemente</a> <BsGithub className={css.icon} /> <br />
-                    <a href="https://github.com/arcadioreyes/" target="_blank">Arcadio Reyes</a> <BsGithub className={css.icon}/>
+                    <a href="https://github.com/axelclemente?tab=repositories" rel="noreferrer" target="_blank">Axel Clemente</a> <BsGithub className={css.icon} /> <br />
+                    <a href="https://github.com/arcadioreyes/" rel="noreferrer" target="_blank">Arcadio Reyes</a> <BsGithub className={css.icon}/>
                 </span>
             </div>
 
@@ -40,6 +52,18 @@ const Footer = () => {
                     <p>Frontend: React.js, Css Sass.</p>
                     <p className={css.test}>Backend: Node.js, Express, Mongoose.</p>
                 </div>
+            </div>
+
+            <div>
+                <h3>List of Strolls</h3>
+                {strolls.map(stroll =>{
+                    return <div key={stroll._id}>
+                        <h3>{stroll.title}</h3>
+                        <p>{stroll.description}</p>
+                        <img src={stroll.img} alt={"stroll"} />
+                        <p>{stroll.duration}</p>
+                    </div>
+                }) }
             </div>
 
         </motion.div>
