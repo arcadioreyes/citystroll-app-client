0967c83ce9faed3119bc91d2fd692e935abb8a10
arcadioreyes <109815910+arcadioreyes@users.noreply.github.com>
2023-02-09 23:22:32 +0100

    form at SignUpPage now is working and connected to MongoDB. User can be created at the DB. very happy! :)

--- src/components/SignUpPage/SignUpPage.jsx
+++ src/components/SignUpPage/SignUpPage.jsx
@@ -1,5 +1,6 @@
-import React from "react";
 import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const SignUpPage = () => {
   const [user, setUser] = useState({
@@ -10,10 +11,22 @@ const SignUpPage = () => {
     city: "",
   });
 
-  const handleChange = (e) => {};
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    setUser({ ...user, [name]: value });
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    console.log(user);
+    axios
+      .post("http://localhost:5005/auth/signup", user)
+      .then(() => navigate("/loginpage"))
+      .catch((err) => console.error(err));
   };
 
   return (
@@ -21,37 +34,59 @@ const SignUpPage = () => {
       <h1>Welcome Stroller to the Sign Up Page!</h1>
       <br />
 
-      <form>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email:</label>
+        <br />
         <input
           type="text"
           name="email"
           value={user.email}
           onChange={handleChange}
         />
+        <br />
+
+        <label htmlFor="username">Username:</label>
+        <br />
         <input
           type="text"
           name="username"
           value={user.username}
           onChange={handleChange}
         />
+        <br />
+
+        <label htmlFor="password">Password:</label>
+        <br />
         <input
           type="password"
-          name="username"
-          value={user.username}
+          name="password"
+          value={user.password}
           onChange={handleChange}
         />
+        <br />
+
+        <label htmlFor="country">Country:</label>
+        <br />
         <input
           type="text"
           name="country"
           value={user.country}
           onChange={handleChange}
         />
+        <br />
+
+        <label htmlFor="city">City:</label>
+        <br />
+
         <input
           type="text"
           name="city"
           value={user.city}
           onChange={handleChange}
         />
+        <br />
+        <br />
+
         <button type="submit">Create my account</button>
       </form>
     </div>
