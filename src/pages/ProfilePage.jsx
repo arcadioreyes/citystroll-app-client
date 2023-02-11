41d69a688623396cc6bd33bf9bd450370c5cd4d2
arcadioreyes <109815910+arcadioreyes@users.noreply.github.com>
2023-02-11 18:02:07 +0100

    Update profile picture function added. NOT WORKING PROPERLY

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,10 +1,51 @@
-import { useContext } from "react";
+import { useState, useContext } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../context/auth.context";
 import placeholderProfilePicture from "../images/avatar.png";
 
 const ProfilePage = () => {
-  const { isLoggedIn, user } = useContext(AuthContext);
+  const [imageUrl, setImageUrl] = useState("");
+  const { isLoggedIn, user, setUser } = useContext(AuthContext);
+
+  const handleFileUpload = (e) => {
+    // console.log("The file to be uploaded is: ", e.target.files[0]);
+
+    const uploadData = new FormData();
+
+    // imageUrl => this name has to be the same as in the model since we pass
+    // req.body to .create() method when creating a new movie in '/api/movies' POST route
+    uploadData.append("imageUrl", e.target.files[0]);
+
+    // service
+    //   .uploadImage(uploadData)
+    axios
+      .post("http://localhost:5005/api/upload", uploadData)
+      .then((response) => {
+        // console.log("response is: ", response);
+        // response carries "fileUrl" which we can use to update the state
+        setImageUrl(response.data.fileUrl);
+      })
+      .catch((err) => console.log("Error while uploading the file: ", err));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const storedToken = localStorage.getItem("authToken");
+
+    axios.put(
+      "http://localhost:5005/api/users",
+      { image: imageUrl },
+      { headers: { Authorization: `Bearer ${storedToken}` } }
+        .then((response) => {
+          console.log("put response data", response.data);
+          setUser(response.data);
+          setImageUrl("");
+        })
+
+        .catch((err) => console.log(err))
+    );
+  };
 
   return (
     <div>
@@ -20,10 +61,22 @@ const ProfilePage = () => {
                 ? user.profilePicture
                 : placeholderProfilePicture
             }
-            alt={"User's Profile picture"}
+            alt={"User's Profile"}
             className="profilePicture"
           />
           <br />
+
+          <h2>Update image form</h2>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="file"
+              name="imageUrl"
+              onChange={(e) => handleFileUpload(e)}
+            />
+            <br />
+            <br />
+            <button type="submit">Update User Image</button>
+          </form>
         </>
       )}
 
