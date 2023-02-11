e85351793ee32e0faf60b099342f6884834a7fc7
arcadioreyes <109815910+arcadioreyes@users.noreply.github.com>
2023-02-11 11:03:27 +0100

    AuthProviderWrapper imported in index.js. added it as a wrapper for App

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css' //pasted
-import './App.scss'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css"; //pasted
+import "./App.scss";
+import "./index.css";
 
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from "react-router-dom";
+import { AuthProviderWrapper } from "./context/auth.context";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
-      <App />
-    </Router>   
-  </React.StrictMode>,
-)
+      <AuthProviderWrapper>
+        <App />
+      </AuthProviderWrapper>
+    </Router>
+  </React.StrictMode>
+);
 
 // Esto es el main.jsx
