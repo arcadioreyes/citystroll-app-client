67aa458b70a9629e3457699e02ac3f9c16af951e
arcadioreyes <109815910+arcadioreyes@users.noreply.github.com>
2023-02-13 17:00:06 +0100

    added a Home button at Header if isLoggedIn

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -45,6 +45,12 @@ const Header = () => {
           {isLoggedIn && (
             <>
               <p>Welcome back {user.username}!</p>
+
+              <Link to={"/"}>
+                {" "}
+                <button>Home</button>{" "}
+              </Link>
+
               <Link to={"/profile"}>
                 {" "}
                 <button>Profile</button>{" "}
